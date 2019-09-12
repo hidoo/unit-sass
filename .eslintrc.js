cec9478d362bc50aceb74a206c68a590993e5b8f
hidoo <hidoooo@gmail.com>
2019-09-12 19:43:59 +0900

    chore(eslint): update @hidoo/eslint-config to version 0.2.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,29 @@
 module.exports = {
-  extends: [
-    '@hidoo/eslint-config'
+  'root': true,
+  'extends': [
+    '@hidoo/eslint-config',
+    '@hidoo/eslint-config/+babel'
+  ],
+  'overrides': [
+    // for Mocha
+    {
+      'files': [
+        '**/*.test.js'
+      ],
+      'extends': [
+        '@hidoo/eslint-config/+mocha'
+      ]
+    },
+    // for Node
+    {
+      'files': [
+        '**/gulpfile.babel.js',
+        '**/task/*.js',
+        'packages/sassdoc-to-markdown/src/**/*.js'
+      ],
+      'extends': [
+        '@hidoo/eslint-config/+node'
+      ]
+    }
   ]
 };
